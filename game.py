f404126313d6578a35b3f4dafdc2c7958564a53d
GWStuart <89712016+GWStuart@users.noreply.github.com>
2024-12-07 23:54:25 +1000

    added pygame frontend

---
+++ game.py
@@ -0,0 +1,58 @@
+from gameLogic import Game
+import pygame
+pygame.init()
+
+LENGTH, HEIGHT = 800, 600
+
+win = pygame.display.set_mode((LENGTH, HEIGHT))
+pygame.display.set_caption("Snake Game")
+
+snake_direction = "r"  # the starting snake direction
+
+CELL_SIZE = 10
+game = Game(LENGTH, HEIGHT, CELL_SIZE)
+clock = pygame.time.Clock()
+
+def render(game: Game) -> None:
+    win.fill((0, 0, 0))
+
+    for part in game.getSnakeBody():
+        pygame.draw.rect(win, (0, 255, 0), (*part, CELL_SIZE, CELL_SIZE))
+
+    apple = game.getApplePosition()
+    pygame.draw.rect(win, (255, 0, 0), (*apple, CELL_SIZE, CELL_SIZE))
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        game.sendKey("r")
+        snake_direction = "r"
+    elif keys[pygame.K_a]:
+        game.sendKey("l")
+        snake_direction = "l"
+    elif keys[pygame.K_w]:
+        game.sendKey("u")
+        snake_direction = "u"
+    elif keys[pygame.K_s]:
+        game.sendKey("d")
+        snake_direction = "d"
+    else:
+        game.sendKey(snake_direction)
+
+    if not game.update():
+        print("GAME OVER")
+        run = False
+
+    render(game)
+
+    pygame.display.update()
+    clock.tick(10)
+
--- gameLogic.py
+++ gameLogic.py
@@ -1,12 +1,21 @@
+import random
 
 class Game:
     SNAKE_SPAWN_POSITION = (20, 20)
 
-    def __init__(self, LENGTH: int, HEIGHT: int):
+    def __init__(self, LENGTH: int, HEIGHT: int, CELL_SIZE: int = 10):
         self.LENGTH = LENGTH
         self.HEIGHT = HEIGHT
+        self.CELL_SIZE = 10
 
-        self.snake = Snake(LENGTH, HEIGHT, *Game.SNAKE_SPAWN_POSITION)
+        self.snake = Snake(LENGTH, HEIGHT, CELL_SIZE, *Game.SNAKE_SPAWN_POSITION)
+
+    """
+    Updates the game and returns False if the game is over
+    @returns False if the game is over and True otherwise
+    """
+    def update(self) -> bool:
+        return self.snake.update()
 
     """
     Processes the given key press in the game.
@@ -33,21 +42,29 @@ class Game:
     def getSnakeBody(self) -> list[tuple[int]]:
         return self.snake.body
 
+    def getApplePosition(self) -> tuple[int, int]:
+        return self.snake.apple
+
 
 class Snake:
-    CELL_SIZE = 10
-
     """
     Constructor for the snake
     @param x, y are the x and y initial coordinates of the snake
     """
-    def __init__(self, LENGTH: int, HEIGHT: int, x: int, y: int):
+    def __init__(self, LENGTH: int, HEIGHT: int, CELL_SIZE: int, x: int, y: int):
         self.LENGTH = LENGTH
         self.HEIGHT = HEIGHT
+        self.XCELLS = LENGTH // CELL_SIZE
+        self.YCELLS = HEIGHT // CELL_SIZE
+        self.CELL_SIZE = CELL_SIZE
         self.x = x
         self.y = y
+        self.apple = self.getRandomCell()
 
         self.body = [(x, y)]
+
+    def getRandomCell(self) -> tuple[int, int]:
+        return random.randint(0, self.XCELLS) * self.CELL_SIZE, random.randint(0, self.YCELLS) * self.CELL_SIZE
 
     """
     Moves the snake in its direction of motion
@@ -56,15 +73,39 @@ class Snake:
     def move(self, direction: str):
         match direction:
             case "l":
-                self.x -= Snake.CELL_SIZE
+                self.x -= self.CELL_SIZE
             case "r":
-                self.x += Snake.CELL_SIZE 
+                self.x += self.CELL_SIZE 
             case "u":
-                self.y -= Snake.CELL_SIZE
+                self.y -= self.CELL_SIZE
             case _:
-                self.y += Snake.CELL_SIZE
+                self.y += self.CELL_SIZE
 
         self.body.append((self.x, self.y))
 
-        self.body.pop(0)  # remove the last element
+    """
+    Checks that the game is not over and checks if an apple has been eaten.
+    This method should always be run after the snake as moved. I.e. after calling the move() method
+    @returns False if the game is over and True otherwise
+    """
+    def update(self) -> bool:
+        if self.isGameOver():
+            return False
+
+        if self.body[-1] == self.apple:  # check if the snake is on the apple
+            self.apple = self.getRandomCell()
+        else:
+            self.body.pop(0)
+        return True
+
+    """
+    checks if the game is over
+    @returns True if the game is over and False otherwise
+    """
+    def isGameOver(self) -> bool:
+        if self.x < 0 or self.x > self.LENGTH or self.y < 0 or self.y > self.HEIGHT:
+            return True
+        if self.body[-1] in self.body[:-1]:  # might be faster to compare body vs set() of body idk
+            return True
+        return False
 
